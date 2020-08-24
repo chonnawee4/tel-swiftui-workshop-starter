9583975a0c20f8831e76335c519541e49887cc70
Chonnawee <chonnawee.ch@gmail.com>
2020-08-24 15:06:57 +0700

    Supoort Xcode11

--- SwiftUIWorkShop/AppDelegate.swift
+++ SwiftUIWorkShop/AppDelegate.swift
@@ -4,11 +4,8 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
